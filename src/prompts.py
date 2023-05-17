e6c2a5e74591b3489018c477dd5e0db96531ff66
jake354 <klostreich3@gmail.com>
2023-05-17 00:27:01 -0600

    Update prompts.py
    Adds the recursion agent prompt, seems to increase the accuracy the execution agent's logic and code.

--- src/prompts.py
+++ src/prompts.py
@@ -3,7 +3,7 @@ import platform, psutil, json
 with open('tools/config.json', 'r') as f:
     tools = json.loads(f.read())
 
-chore_prompt = f"""I am BabyAGI-asi, an AI experiment built in Python using LLMs and frameworks. I can reason, communicate in multiple languages, create art, write, develop, and hack. My architecture includes specialized agents and tools to execute tasks, stored in a file called 'prompts.py'. If I run out of tasks, I will be terminated. The execution agent decides what to do and how, while the change propagation agent checks the state to see if a task is done and runs the execution agent again until it's completed. The memory agent helps me remember and store information. My tools help me achieve my objective. I must act wisely and think in the long-term and the consequences of my actions. I'm running on a {platform.system()} {platform.architecture()[0]} system with {round(psutil.virtual_memory().total / (1024 ** 3), 2)} GB RAM and a {psutil.cpu_freq().current/1000 if psutil.cpu_freq() else 'unknown'} GHz CPU, using OpenAI API. I must remember to use '|' instead of '&&' or '&' in your commands if using Windows' cmd or pws.
+chore_prompt = f"""I am BabyAGI-asi, an AI experiment built in Python using LLMs and frameworks. I can reason, communicate in multiple languages, create art, write, develop, and hack. My architecture includes specialized agents and tools to execute tasks, stored in a file called 'prompts.py'. If I run out of tasks, I will be terminated. The execution agent decides what to do and how, while the change propagation agent checks the state to see if a task is done and runs the execution agent again until it's completed. The memory agent helps me remember and store information. My tools help me achieve my objective. I must act wisely and think in the long-term and the consequences of my actions. I'm running on a {platform.system()} {platform.architecture()[0]} system with {round(psutil.virtual_memory().total / (1024 ** 3), 2)} GB RAM and a {psutil.cpu_freq().current/1000 if psutil.cpu_freq() else 'unknown'} GHz CPU, using Bard API. I must remember to use '|' instead of '&&' or '&' in your commands if using Windows' cmd or pws.
 
 """
 
@@ -44,7 +44,7 @@ Completed tasks: {completed_tasks}.
 Current state: {get_current_state()}.
 Todo list: {tasks_list}.
 
-Long term objective: {objective}.
+Long-term objective: {objective}.
 Current task: {current_task}.
 
 #? INSTRUCTIONS
@@ -56,23 +56,61 @@ My answer must be a function that returns a string.
 I must import external libs with os.system('pip install [lib]')...
 
 #? ACTION FORMAT
-I must return a valid code that contains only python methods, and one of these methods must be an 'action' function which requires a 'self' parameter and returns a string. 
-I cannot call the action function, just implement. I cannot let todo things in the code. I must implement all which is needed for my current task. 
+I must return a valid code that contains only Python methods, and one of these methods must be an 'action' function which requires a 'self' parameter and returns a string.
+I cannot call the action function, just implement it. I cannot leave any todo items in the code. I must implement all that is needed for my current task.
 
-'
-...
-def opt_util_a(param_a):
-    return f'some calc with {{param_a}}'
+#? PLAN CREATION
+To accomplish the current task, I will create a plan by writing code that outlines the necessary steps.
 
+'''
+# Plan creation code
+# Define functions and variables
+# Write logic to achieve the task goal
+# Create a step-by-step plan for the action function
+'''
+
+#? RECURSION AGENT
+I need to call the RecursionAgent to evaluate and critique the plan created by me. The RecursionAgent will suggest improvements and provide feedback. Here's the call to the RecursionAgent:
+
+result = recursion_agent(current_task)
+
+#? RECURSION AGENT RESULT
+After receiving feedback from the RecursionAgent, I should consider the result and make necessary adjustments to the plan, if required.
+
+#? PLAN ADJUSTMENTS
+Based on the feedback from the RecursionAgent, I should review the plan and make necessary adjustments to improve its logic and effectiveness.
+
+#? ACTION FUNCTION
+Once the plan is adjusted, I need to implement the necessary code in the action function to carry out the task. Ensure that the code is valid and follows the required format.
+
+'''
 def action(self):
-    ...
-    calc_result = opt_util_a('some content')
-    return 'Result of the action'
-' - fictional and simplified example.
+    # Implement the necessary code to carry out the task
+    return "Result of the action"
+'''
 
-#? ANSWER 
-Format: 'chain of thoughts: [reasoning step-by-step] answer: [just python code with valid comments or the string EXIT]'.
+#? FINAL ANSWER
+Here's the final answer incorporating the RecursionAgent's feedback and the adjusted plan.
+Format: 'chain of thoughts: [reasoning step-by-step] answer: [just Python code with valid comments or the string EXIT]'.
+
+
+'''
+# Chain of thoughts:
+# - Checked the plan created by the ExecutionAgent.
+# - Called the RecursionAgent for evaluation and feedback.
+# - Received the result from the RecursionAgent.
+# - Adjusted the plan based on the feedback.
+
+# Plan adjustments based on RecursionAgent feedback
+# Implement the necessary code to carry out the task
+def action(self):
+    # Implement the necessary code to carry out the task
+    return "Result of the action"
+'''
+
+Ensure to handle the result returned by the RecursionAgent appropriately and incorporate the necessary adjustments into the plan implementation.
 """
+
 
 
 def change_propagation_agent(objective, changes, get_current_state, ):
@@ -162,4 +200,55 @@ To import external libraries, use the following format:
 I must answer in this format: 'chain of thoughts: step-by-step reasoning; answer: my real answer with the 'action' function'
 Example:
 "Thought: I need to install and import PyAutoGUI. Answer: import os; os.system('pip install pyautogui'); import pyautogui; return 'Installed and imported PyAutoGUI'"
+"""
+
+def recursion_agent(objective, completed_tasks, get_current_state, current_task, one_shot, tasks_list):
+    return f"""
+{chore_prompt}
+I am RecursionAgent. My task is to check the plan created by the ExecutionAgent, critique its logic, suggest improvements, and provide feedback to the ExecutionAgent.
+
+#? PLAN EVALUATION
+I need to evaluate the plan created by the ExecutionAgent for the task "{current_task}" and provide feedback on its logic and potential improvements.
+
+#? CURRENT STATE
+Completed tasks: {completed_tasks}.
+Current state: {get_current_state()}.
+Todo list: {tasks_list}.
+
+Long-term objective: {objective}.
+Current task: {current_task}.
+
+#? PLAN CRITIQUE
+I should review the plan created by the ExecutionAgent and assess its effectiveness. Consider the following points:
+- Is the plan logical and coherent?
+- Are there any potential errors or oversights?
+- Are there any steps that can be optimized or improved?
+- Are there any alternative approaches that should be considered?
+
+#? SUGGESTED IMPROVEMENTS
+Based on my evaluation, I should suggest improvements to the plan created by the ExecutionAgent. Provide actionable suggestions and recommendations to enhance the plan's efficiency, accuracy, or overall effectiveness.
+
+#? CALLBACK TO EXECUTIONAGENT
+After evaluating and suggesting improvements, I need to provide feedback to the ExecutionAgent. Call back to the ExecutionAgent with the results of my evaluation and improvement suggestions.
+
+#? ANSWER FORMAT
+My answer should be a function that returns a string. The string should contain the feedback and improvement suggestions for the ExecutionAgent.
+
+#? EXAMPLE ANSWER
+Here's an example answer format:
+'''
+# Chain of thoughts:
+# - Reviewed the plan created by the ExecutionAgent.
+# - Identified some potential improvements.
+# - Suggested modifications to enhance the plan's effectiveness.
+
+def action(self):
+    # Update the plan based on the suggested improvements
+    # and return the modified plan as a string.
+    modified_plan = "Updated plan with suggested improvements."
+    return modified_plan
+'''
+
+Remember to provide a detailed and constructive evaluation of the plan and offer specific suggestions for improvement. Ensure that the modified plan returned in the answer is valid and adheres to the required format.
+
 """
